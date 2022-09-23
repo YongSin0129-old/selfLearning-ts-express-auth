4b8d7031e16aafbe9c338c04da4b8adff81d1b1d
yongsin0129 <yongsin0129@gmail.com>
2022-09-23 11:38:36 +0800

    feat : add error handler

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,4 +9,35 @@ router.post('/test', (req, res, next) => {
   res.send(JSON.stringify(req.body))
 })
 
+/********************************************************************************
+*
+          測試 error handle
+*
+*********************************************************************************/
+// 錯誤處理 1 直接顯示錯誤
+// router.get('/error', (req, res, next) => {
+//   throw 'error page.';
+// });
+
+// 錯誤處理 2 使用 next 下傳
+
+router.get('/data/error', (req, res, next) => {
+  // Fake API
+  const getProfile = new Promise((resolve, reject) => {
+    setTimeout(() => resolve({ name: 'howhow', age: 25 }), 100)
+  })
+  const getFriends = new Promise((resolve, reject) => {
+    setTimeout(() => resolve([]), 120)
+  })
+  const errorRequest = new Promise((resolve, reject) => {
+    setTimeout(() => reject('Oops ! error !!!'), 1000)
+  })
+
+  getProfile
+    .then(profile => getFriends)
+    .then(friends => errorRequest)
+    .then(() => res.send('GoGoGo!'))
+    .catch(err => next(err))
+})
+
 export default router
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import path from 'path'
 import dotenv from 'dotenv'
 
-import appRoute from './app/app.routing';
+import appRoute from './app/app.routing'
 
 const app = express()
 
 // 包山包海法 , 每當有資料進來的時候都會去執行它，這樣的做法是最簡單的，但也是比較不好的做法 ：
 // 個別使用法的做法是 最推薦的 ex : router.post('/', express.json(), (req, res) => {} )
-app.use(express.json());
+app.use(express.json())
 
 // 動態選擇環境變數的檔案 , 因為 prod , dev 路徑不同，需用 ../退到根目錄再指定 src 資料夾內的 env
 dotenv.config({
@@ -19,12 +19,16 @@ dotenv.config({
 })
 
 // route 入口
-app.use('/', appRoute);
+app.use('/', appRoute)
 
 app.get('/', (req, res, next) => {
   res.send('Hello, Express')
 })
 
+// global error message handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message || err })
+})
 
 // 監控 process.env.PORT
 app.listen(process.env.PORT, () =>
