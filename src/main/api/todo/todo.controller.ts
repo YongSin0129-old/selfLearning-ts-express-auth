080c32915008bf4c77ea8d6850becf9f7ec91272
yongsin0129 <yongsin0129@gmail.com>
2022-09-24 11:09:45 +0800

    refactor : Controller 物件架構化

---
+++ src/main/api/todo/todo.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express'
+import { ControllerBase } from '../../../bases/controller.base'
+import { HttpStatus } from '../../../types/response.type'
+
+export class TodoController extends ControllerBase {
+  public async getTodos (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    const obj = this.formatResponse([], HttpStatus.OK)
+    res.status(obj.status).json(obj)
+  }
+}
--- src/main/api/todo/todo.routing.ts
+++ src/main/api/todo/todo.routing.ts
@@ -1,11 +1,19 @@
 import { RouteBase } from '../../../bases/route.base'
+import { TodoController } from './todo.controller'
 
 export class TodoRoute extends RouteBase {
+  protected controller!: TodoController
+
   constructor () {
     super()
   }
 
-  protected registerRoute (): void {
-    this.router.get('/test', (req, res, next) => res.send('todo test.'))
+  protected initial (): void {
+    this.controller = new TodoController()
+    super.initial()
+  }
+
+  protected registerRoute(): void {
+    this.router.get('/', (req ,res, next) => this.controller.getTodos(req, res, next));
   }
 }
